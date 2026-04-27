d3deb1aa3bff11a6fa9a7a2b2313e75134e657c6
vodozbor-dev <seroga2004odeuchuk@gmail.com>
2026-04-27 20:11:08 +0200

    Update MiniBank.sol

--- MiniBank.sol
+++ MiniBank.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.8.0;
 
+contract MiniBank {
+
+    uint public balance;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        balance += msg.value;
+    }
+
+    function withdraw(uint _amount) public {
+        require(msg.sender == owner, "Not owner");
+        require(_amount <= balance, "Not enough money");
+
+        balance -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+}
